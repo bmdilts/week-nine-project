5cc3044c6e14753870d4c8d35bf2d82d2bc862ef
bmdilts <bmdilts@gmail.com>
2017-07-21 14:54:13 -0400

    react app created

---
+++ components/PlayList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayListItem from './PlayListItem.js'
+
+export default class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>MyComponent</div>
+    );
+  }
+}
